c138a76a9bd225b4e6e1e3f7f25b4fa1a2ca3f2a
Evan Montembeault <evan@emonte.net>
2022-05-29 14:39:45 -0700

    basic setup

---
+++ src/Main.hx
@@ -0,0 +1,14 @@
+package;
+using Interlude;
+
+class Main {
+    public static function main() {
+        /*
+        read files
+        lexer
+        parser
+        transforms
+        write output
+        */
+    }
+}
